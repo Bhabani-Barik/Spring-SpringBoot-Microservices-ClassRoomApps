75b782d59683794206ba2b80176f6dfdb7423af9
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-12-12 22:20:38 +0530

    Proj_05_IOC_DependencyInjectionXml-AnnotationCfgs

---
+++ Proj_05_IOC_DependencyInjectionXml-AnnotationCfgs/src/com/kanha/sbeans/WishMessageGenerator.java
@@ -0,0 +1,36 @@
+package com.kanha.sbeans;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("wmg")
+public class WishMessageGenerator {
+
+	@Autowired // Field Injection
+	private LocalTime localTime; // HAS-A property
+
+	@Autowired
+	private LocalDate localDate; // HAS-A property
+
+	// Business Method
+	public String generateWishMessage(String user) {
+		System.out.println("WishMessageGenerator.generateWishMessage():: " + localTime + " ..... " + localDate);
+
+		// get current hour of the day
+		int hour = localTime.getHour(); // in 24hrs format
+
+		if (hour < 12) {
+			return "Good Morning :: " + user;
+		} else if (hour < 16) {
+			return "Good Afternoon :: " + user;
+		} else if (hour < 20) {
+			return "Good Evening :: " + user;
+		} else {
+			return "Good Night :: " + user;
+		}
+	}
+
+}
