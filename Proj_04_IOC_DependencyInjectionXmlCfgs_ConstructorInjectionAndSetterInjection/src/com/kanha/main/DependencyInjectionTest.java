dc71e01bca41c955835f28f9cde5207a4a639695
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-12-12 22:19:58 +0530

    Proj_04_IOC_DependencyInjectionXmlCfgs_ConstructorInjectionAndSetterInjection

---
+++ Proj_04_IOC_DependencyInjectionXmlCfgs_ConstructorInjectionAndSetterInjection/src/com/kanha/main/DependencyInjectionTest.java
@@ -0,0 +1,41 @@
+package com.kanha.main;
+
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.kanha.sbeans.WishMessageGenerator;
+
+public class DependencyInjectionTest {
+	
+	public static void main(String[] args) {
+		System.out.println("DependencyInjectionTest.main():: Start");
+		
+		// create IOC container
+		FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("src/com/kanha/configurations/applicationContext.xml");
+		
+		// get target spring bean class object ref from the IOC container
+		Object obj = ctx.getBean("wmg");
+		
+		// TypeCasting
+		WishMessageGenerator generator = (WishMessageGenerator)obj;
+		
+		// Invoke business method
+		String result = generator.generateWishMessage("Shwetapurna");
+		
+		System.out.println(result);
+		
+		// Close the IOC container
+		ctx.close();
+		
+		System.out.println("DependencyInjectionTest.main():: End");
+		
+	}
+
+}
+
+/* 
+ 	Q: If we enable	both setter injection and constructor injection on target spring bean property having two different sets of values.
+ 		Can you tell us, which value will be injected as the final value?
+ 		
+ 		Answer: Setter injection value override the value injected by constructor injection.
+ 
+ */
---
+++ Proj_04_IOC_DependencyInjectionXmlCfgs_ConstructorInjectionAndSetterInjection/src/com/kanha/sbeans/WishMessageGenerator.java
@@ -0,0 +1,53 @@
+package com.kanha.sbeans;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class WishMessageGenerator {
+	
+	private LocalTime localTime; // HAS-A property
+	private LocalDate localDate; // HAS-A property
+	
+		
+	// For Constructor Injection :: 2-parameter constructor
+	public WishMessageGenerator(LocalTime localTime, LocalDate localDate) {
+		System.out.println("WishMessageGenerator :: 2-param constructor");
+		this.localTime = localTime;
+		this.localDate = localDate;
+	}
+	
+	// Setter method for Setter Injection
+	public void setLocalTime(LocalTime localTime) {
+		System.out.println("WishMessageGenerator.setLocalTime()");
+		this.localTime = localTime;
+	}
+	
+	public void setLocalDate(LocalDate localDate ) {
+		System.out.println("WishMessageGenerator.setLocalDate()");
+		this.localDate = localDate;
+	}
+	
+	
+	// Business Method
+	public String generateWishMessage(String user) {
+		System.out.println("WishMessageGenerator.generateWishMessage():: " + localTime +" ..... " + localDate );
+		
+		// get current hour of the day
+		int hour = localTime.getHour(); // in 24hrs format
+		
+		if (hour < 12) {
+			return "Good Morning :: " + user;
+		} 
+		else if (hour < 16) {
+			return "Good Afternoon :: " + user;
+		}
+		else if (hour < 20) {
+			return "Good Evening :: " + user;
+		}
+		else {
+			return "Good Night :: " + user;
+		}
+	}
+	
+
+}
