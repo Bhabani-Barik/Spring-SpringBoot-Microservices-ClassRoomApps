ad8f86cb5140107c8b68e96e107d78551b3736fa
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-12-13 11:22:55 +0530

    Proj_06_IOC_DependencyInjection-JavaConfigApproach

---
+++ Proj_06_IOC_DependencyInjection-JavaConfigApproach/src/com/kanha/main/DependencyInjectionTest.java
@@ -0,0 +1,44 @@
+package com.kanha.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.kanha.configurations.AppConfiguration;
+import com.kanha.sbeans.WishMessageGenerator;
+
+public class DependencyInjectionTest {
+
+	public static void main(String[] args) {
+		System.out.println("DependencyInjectionTest.main():: Start");
+
+		// create IOC container
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfiguration.class);
+
+		// get target spring bean class object ref from the IOC container
+		Object obj = ctx.getBean("wmg");
+
+		// TypeCasting
+		WishMessageGenerator generator = (WishMessageGenerator) obj;
+
+		// Invoke business method
+		String result = generator.generateWishMessage("Purnima");
+
+		System.out.println(result);
+
+		// Close the IOC container
+		ctx.close();
+
+		System.out.println("DependencyInjectionTest.main():: End");
+
+	}
+
+}
+
+/*
+ * Q: If we enable both setter injection and constructor injection on target
+ * spring bean property having two different sets of values. Can you tell us,
+ * which value will be injected as the final value?
+ * 
+ * Answer: Setter injection value override the value injected by constructor
+ * injection.
+ * 
+ */
