1a80b86f92b960f64cb5c1a2fb679eed43ac065d
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-12-13 21:37:24 +0530

    ../Proj_40_Boot_LayeredAppMiniProject-MySQL/

---
+++ Proj_40_Boot_LayeredAppMiniProject-MySQL/src/main/java/com/kanha/Proj40BootLayeredAppMiniProjectMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.kanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj40BootLayeredAppMiniProjectMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj40BootLayeredAppMiniProjectMySqlApplication.class, args);
+	}
+
+}
