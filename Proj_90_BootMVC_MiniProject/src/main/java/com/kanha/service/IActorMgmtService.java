d7d01a92960dfe27f858bad12c03d48a28fd3126
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-12-13 22:00:57 +0530

    ../Proj_90_BootMVC_MiniProject/

---
+++ Proj_90_BootMVC_MiniProject/src/main/java/com/kanha/service/IActorMgmtService.java
@@ -0,0 +1,13 @@
+package com.kanha.service;
+
+import java.util.List;
+
+import com.kanha.vo.ActorVO;
+
+public interface IActorMgmtService {
+	   public  List<ActorVO>  showAllActorDetails();
+	   public  String   saveActor(ActorVO actorVO);
+	   public   ActorVO   showActorById(int id);
+	   public   String   editActor(ActorVO  actorVO);
+	   public  String    deleteActorById(int aid);
+	}
---
+++ Proj_90_BootMVC_MiniProject/src/main/java/com/kanha/vo/ActorVO.java
@@ -0,0 +1,26 @@
+package com.kanha.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ActorVO {
+	 // Data properties
+	private  Integer aid;
+	@NonNull
+	private String  aname;
+	@NonNull
+	private   String  addrs;
+	@NonNull
+	private  String category;
+	@NonNull
+	private   Double remuneration;
+	
+}
+
