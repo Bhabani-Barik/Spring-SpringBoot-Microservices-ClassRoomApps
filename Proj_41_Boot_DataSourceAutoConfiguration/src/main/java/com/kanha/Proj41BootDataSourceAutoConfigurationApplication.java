49b198a47f5db2d7a0635e319f45d43460e9bc95
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-12-13 21:39:05 +0530

    ../Proj_41_Boot_DataSourceAutoConfiguration/

---
+++ Proj_41_Boot_DataSourceAutoConfiguration/src/main/java/com/kanha/Proj41BootDataSourceAutoConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.kanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj41BootDataSourceAutoConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj41BootDataSourceAutoConfigurationApplication.class, args);
+	}
+
+}
