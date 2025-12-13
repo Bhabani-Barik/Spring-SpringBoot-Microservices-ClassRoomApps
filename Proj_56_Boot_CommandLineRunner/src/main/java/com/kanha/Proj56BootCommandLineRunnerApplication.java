fd0223f77fc62b8f4eaa418124b51403931058df
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-12-13 21:45:56 +0530

    ../Proj_56_Boot_CommandLineRunner/

---
+++ Proj_56_Boot_CommandLineRunner/src/main/java/com/kanha/Proj56BootCommandLineRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.kanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj56BootCommandLineRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj56BootCommandLineRunnerApplication.class, args);
+	}
+
+}
